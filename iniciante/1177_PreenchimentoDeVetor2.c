0742f35d6e0fc47bbcecb41dd9bfb240bb8e5335
MATEUS-fr12 <franca12312martins@gmail.com>
2026-06-05 20:30:43 -0300

    +code

--- iniciante/1177_PreenchimentoDeVetor2.c
+++ iniciante/1177_PreenchimentoDeVetor2.c
@@ -1,1 +1,12 @@
-///
+///PREENCHIMENTO DE VETOR 2///
+#include <stdio.h>
+int main() {
+    int T, i;
+    int N[1000];
+    scanf("%d", &T);
+    for(i = 0; i < 1000; i++) {
+        N[i] = i % T; 
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+    return 0;
+}
