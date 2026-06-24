2a97776def4bf74c2623326f42c474b7a8c1bf69
MATEUS-fr12 <franca12312martins@gmail.com>
2026-06-23 23:09:01 -0300

    code++

---
+++ iniciante/1187_AreaSuperior.c
@@ -0,0 +1,23 @@
+///AREA SUPERIOR
+#include <stdio.h>
+int main(){
+    char O;
+    double M[12][12];
+    double resultado = 0.0;
+    scanf(" %c", &O);
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+    for(int i = 0; i <= 4; i++){
+        for(int j = i + 1; j < 11 - i; j++){
+            resultado += M[i][j];
+        }
+    }
+    if(O == 'M'){
+        resultado /= 30.0;
+    }
+    printf("%.1lf\n", resultado);
+    return 0;
+}
